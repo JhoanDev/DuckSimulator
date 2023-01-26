dbb65b05bd9b318d9235773060e026ecc15dacb0
JhoanDev <jhoandev.log@gmail.com>
2023-01-26 04:29:07 -0300

    First version 1.0

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
-let duck = document.getElementById("duck");
-let viInput = document.getElementById("vi");
-let vfInput = document.getElementById("vf");
-let tInput = document.getElementById("t");
-let initialVelocity = 0;
-let finalVelocity = 0;
-let time = 0;
-let acceleration = 0;
-let distance = 0;
-let currentTime = 0;
-let animation;
+let duck = document.getElementById("duck")
+let viInput = document.getElementById("vi")
+let vfInput = document.getElementById("vf")
+let vatext = document.getElementById("current vel")
+let tptext = document.getElementById("Tempo gasto")
+let atext = document.getElementById("aceleração")
+let initialVelocity = 0
+let finalVelocity = 0
+let time = 0
+let acceleration = 0
+let distance = 750
+let currentTime = 0
+let animation
 
 function start() {
-  initialVelocity = parseFloat(viInput.value);
-  finalVelocity = parseFloat(vfInput.value);
-  time = parseFloat(tInput.value);
-  acceleration = (finalVelocity - initialVelocity) / time;
-  distance = initialVelocity * time + (acceleration * time * time) / 2;
-  currentTime = 0;
-  animation = setInterval(update, 10);
+  initialVelocity = parseFloat(viInput.value)
+  finalVelocity = parseFloat(vfInput.value)
+  time = distance / ((initialVelocity + finalVelocity) / 2)
+  acceleration = (finalVelocity - initialVelocity) / time
+  currentTime = 0
+  atext.innerHTML = `Aceleração: <b>${acceleration.toFixed(2)} (m/s)</b>`
+  animation = setInterval(update, 10)
 }
 
 function update() {
-    currentTime += 0.01;
-    let currentVelocity = initialVelocity + acceleration * currentTime;
-    let currentDistance = initialVelocity * currentTime + (acceleration * currentTime * currentTime) / 2;
-    duck.style.left = currentDistance + "px";
-    if (currentTime >= time) {
-      clearInterval(animation);
+    currentTime += 0.01
+    tptext.innerHTML = `Tempo gasto: <b>${currentTime.toFixed(2)} (s)</b>`
+    let currentVelocity = initialVelocity + acceleration * currentTime
+    vatext.innerHTML = `Velocidade atual: <b>${currentVelocity.toFixed(0)} (m/s)</b></p>`
+    let currentDistance = initialVelocity * currentTime + (acceleration * currentTime * currentTime) / 2
+    duck.style.left = currentDistance + "px"
+    if (currentTime >= time || currentDistance == distance) {
+      clearInterval(animation)
     }
   }
   
