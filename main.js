17cae35d2760fec11e3dfca012ced4880e66104c
JhoanDev <jhoandev.log@gmail.com>
2023-01-26 04:35:35 -0300

    comentarios

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let duck = document.getElementById("duck")
+//criando variaveis e selecionando elementos
+let duck = document.getElementById("duck") 
 let viInput = document.getElementById("vi")
 let vfInput = document.getElementById("vf")
 let vatext = document.getElementById("current vel")
@@ -13,23 +14,23 @@ let currentTime = 0
 let animation
 
 function start() {
-  initialVelocity = parseFloat(viInput.value)
-  finalVelocity = parseFloat(vfInput.value)
-  time = distance / ((initialVelocity + finalVelocity) / 2)
-  acceleration = (finalVelocity - initialVelocity) / time
-  currentTime = 0
-  atext.innerHTML = `Aceleração: <b>${acceleration.toFixed(2)} (m/s)</b>`
-  animation = setInterval(update, 10)
+  initialVelocity = parseFloat(viInput.value) //velocidade inicial
+  finalVelocity = parseFloat(vfInput.value) //velocidade final
+  time = distance / ((initialVelocity + finalVelocity) / 2) //tempo gasto para percorrer os 750M de acordo com vi e vf
+  acceleration = (finalVelocity - initialVelocity) / time //aceleração em m/s
+  currentTime = 0 //tempo inicial
+  atext.innerHTML = `Aceleração: <b>${acceleration.toFixed(2)} (m/s)</b>` //display da acceleração 
+  animation = setInterval(update, 10) //a posição do pato será atualizada a cada 10 milesegundos
 }
 
-function update() {
-    currentTime += 0.01
-    tptext.innerHTML = `Tempo gasto: <b>${currentTime.toFixed(2)} (s)</b>`
-    let currentVelocity = initialVelocity + acceleration * currentTime
-    vatext.innerHTML = `Velocidade atual: <b>${currentVelocity.toFixed(0)} (m/s)</b></p>`
-    let currentDistance = initialVelocity * currentTime + (acceleration * currentTime * currentTime) / 2
-    duck.style.left = currentDistance + "px"
-    if (currentTime >= time || currentDistance == distance) {
+function update() { //função onde a fisica acontece
+    currentTime += 0.01 //tempo atual
+    tptext.innerHTML = `Tempo gasto: <b>${currentTime.toFixed(2)} (s)</b>` //display do tempo atual 
+    let currentVelocity = initialVelocity + acceleration * currentTime //velocidade atual
+    vatext.innerHTML = `Velocidade atual: <b>${currentVelocity.toFixed(0)} (m/s)</b></p>` // //display da velocidade atual
+    let currentDistance = initialVelocity * currentTime + (acceleration * currentTime * currentTime) / 2 //distancia percorrida até o momento
+    duck.style.left = currentDistance + "px" //movimentando o pato de acordo com a distancia
+    if (currentTime >= time || currentDistance == distance) { //caso o pato tenha chegado na distancia ou tempo encerra a função
       clearInterval(animation)
     }
   }
